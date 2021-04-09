6f72d8a25ef7c84222999418626bbd2d47202fc7
Melis <meliszhold@gmail.com>
2021-04-09 17:56:09 +0600

    1

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'antd/dist/antd.css';
-import './index.css';
-import { Provider } from 'react-redux';
-import store from './store/store';
-import App from './components/App/App';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "antd/dist/antd.css";
+import "./index.css";
+import { Provider } from "react-redux";
+import store from "./store/store";
+import App from "./components/App/App";
+console.log("a");
 window.store = store;
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
